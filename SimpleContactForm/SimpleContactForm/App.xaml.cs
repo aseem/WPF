75a873921c2343392be6cb629dbb0fa75af2b7cb
Aseem Kohli <aseemko@gmail.com>
2012-05-24 11:15:19 -0400

    First commit
    Existing code through Chapter 5.

---
+++ SimpleContactForm/SimpleContactForm/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace SimpleContactForm
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
