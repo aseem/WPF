e776956a71e440206c8f1be472b8dc6caca50d8d
Aseem Kohli <aseemko@gmail.com>
2012-05-24 16:14:00 -0400

    Chapter 6 code

---
+++ AutomaticChangeNotification/AutomaticChangeNotification/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutomaticChangeNotification
+{
+   
+    class Person
+    {
+        private string _firstName;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set 
+            { 
+                _firstName = value;
+                OnFirstNameChanged();
+            }
+        }
+
+        public event EventHandler FirstNameChanged;
+
+        private void OnFirstNameChanged()
+        {
+            if (FirstNameChanged != null)
+                FirstNameChanged(this, EventArgs.Empty);
+        }
+
+
+    }
+}
