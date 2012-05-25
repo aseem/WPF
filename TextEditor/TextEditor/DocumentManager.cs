1b555e3e1768dc603146269d85e50cd016297ff9
Aseem Kohli <aseemko@gmail.com>
2012-05-25 11:09:37 -0400

    Text Editor

---
+++ TextEditor/TextEditor/DocumentManager.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using Microsoft.Win32;
+
+namespace TextEditor
+{
+    public class DocumentManager
+    {
+        private string _currentFile;
+        private RichTextBox _textBox;
+        public DocumentManager(RichTextBox textBox)
+        {
+            _textBox = textBox;
+        }
+
+        public bool OpenDocument()
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == true)
+            {
+                _currentFile = dlg.FileName;
+                using (Stream stream = dlg.OpenFile())
+                {
+                    TextRange range = new TextRange(
+                    _textBox.Document.ContentStart,
+                    _textBox.Document.ContentEnd
+                    );
+                    range.Load(stream, DataFormats.Rtf);
+                }
+                return true;
+            }
+            return false;
+           }
+
+        public bool SaveDocument()
+        {
+            if (string.IsNullOrEmpty(_currentFile)) 
+                return SaveDocumentAs();
+            else
+            {
+                using (Stream stream =
+                    new FileStream(_currentFile, FileMode.Create))
+                {
+                    TextRange range = new TextRange(
+                    _textBox.Document.ContentStart,
+                    _textBox.Document.ContentEnd
+                    );
+                    range.Save(stream, DataFormats.Rtf);
+                }
+                return true;
+            }
+        }
+
+        public bool SaveDocumentAs()
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            if (dlg.ShowDialog() == true)
+            {
+                _currentFile = dlg.FileName;
+                return SaveDocument();
+            }
+            return false;
+        }
+    }
+}
---
+++ TextEditor/TextEditor/TextEditorToolbar.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextEditor
+{
+    /// <summary>
+    /// Interaction logic for TextEditorToolbar.xaml
+    /// </summary>
+    public partial class TextEditorToolbar : UserControl
+    {
+        public TextEditorToolbar()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (double i = 8; i < 48; i += 2)
+            {
+                fontSize.Items.Add(i);
+            }
+        }
+    }
+}
